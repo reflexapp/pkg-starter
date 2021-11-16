66f29d84d04f1bd09998777574411687fd0b5d45
Xiaopeng Zhu <nimezhu@gmail.com>
2021-11-16 07:58:37 -0600

    add states as parameter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import pkg from '../package.json';
-import {getNode, GenericPackage} from "@reflexiv/reflexiv"
-
+import {getNode, GenericPackage, GenericContainer} from "@reflexiv/reflexiv"
 const version = pkg.version
-const name = pkg.name.split("/").pop()
+
+const cFirst = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+const cName = (d) => {
+    var a = d.split("/").pop().replace("pkg-","")
+    var n = a.split("-").map((d)=>(cFirst(d)))
+    return n.join(" ")
+}
+const name = cName(pkg.name)
+
 const render = (core, proc) => (container, state) => {
     container.setState(state)
-    var el = container.getElement()
-    var div = getNode(el)
-    div.innerHTML = "Package Starter Here : " + version 
+    var div = getNode(container)
+    div.innerHTML = "Package Starter Here : " + version
     container.on("resize", () => {
-       //container.width and container.height
+        // TODO with container.width and container.height
     })
-    return function(){
-       // close function
+    return function() {
+        // close function
     }
 }
+var _state_ = {}
+// Make Package Init State as a Parameter
+const demo = (state = _state_) => (new GenericPackage(name, version, render, state, []))
 
-const demo = new GenericPackage(name,version,render,{},[])
+// test without services
+demo.test = (el) => {
+    var core = {}
+    var container = new GenericContainer(el)
+    render(core)(container, _state_)
+}
+// TODO add Unit Test Function 
 export default demo
+
